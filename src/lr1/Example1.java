7f27f8d093dd7a25c95e833950d4a7af3ed41c6d
samsonovegor <nice.egor.2004@gmail.com>
2025-12-08 00:14:56 +0500

    Example1.java

--- src/lr1/Example1.java
+++ src/lr1/Example1.java
@@ -1,24 +1,7 @@
-package lr3;
-
-import java.util.Scanner;
+package lr1;
 
 public class Example1 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите число от 1 до 7:");
-        int day = in.nextInt();
-        String name;
-        switch (day) {
-            case 1: name = "Понедельник"; break;
-            case 2: name = "Вторник"; break;
-            case 3: name = "Среда"; break;
-            case 4: name = "Четверг"; break;
-            case 5: name = "Пятница"; break;
-            case 6: name = "Суббота"; break;
-            case 7: name = "Воскресенье"; break;
-            default: name = "Некорректное значение!";
-        }
-        System.out.println(name);
-        in.close();
+    public static void main (String[] args) {
+            System.out.println("Hello Word!");
     }
 }
